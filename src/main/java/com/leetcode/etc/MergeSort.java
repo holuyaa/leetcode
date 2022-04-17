d3394007b20e08d6a29e04c177ba71896eb652eb
Hyoungho Choi <holuyaa@gmail.com>
2022-04-18 00:41:36 +0900

    Merge Sort

--- src/main/java/com/leetcode/etc/MergeSort.java
+++ src/main/java/com/leetcode/etc/MergeSort.java
@@ -3,37 +3,35 @@ package com.leetcode.etc;
 public class MergeSort {
 
     public int[] sort(int[] array) {
-        sort(array, 0, array.length - 1);
-        return array;
+        final int[] tmp = new int[array.length];
+        sort(array, 0, array.length, tmp);
+        return tmp;
     }
 
-    private void sort(int[] array, int left, int right) {
-        if (right == left) return;
+    private void sort(int[] array, int left, int right, int[] tmp) {
+        if (right - left < 2) return;
 
         final int mid = (right + left) / 2;
-        sort(array, left, mid);
-        sort(array, mid + 1, right);
-        merge(array, left, right);
+        sort(array, left, mid, tmp);
+        sort(array, mid, right, tmp);
+        merge(array, left, mid, right, tmp);
     }
 
-    private void merge(int[] array, int left, int right) {
-        final int[] tmp = new int[right - left + 1];
-        final int mid = (right + left) / 2;
-
+    private void merge(int[] array, int left, int mid, int right, int[] tmp) {
         int l = left;
-        int r = mid + 1;
-        int t = 0;
+        int r = mid;
+        int t = left;
 
-        while (l <= mid && r <= right) {
-            tmp[t++] = array[l] <= array[r] ? array[l++] : array [r++];
+        while (l < mid && r < right) {
+            tmp[t++] = array[l] < array[r] ? array[l++] : array[r++];
         }
 
-        if (l <= mid) {
-            System.arraycopy(array, l, tmp, t, mid - l + 1);
+        if (l < mid) {
+            System.arraycopy(array, l, tmp, t, mid - l);
         } else {
-            System.arraycopy(array, r, tmp, t, right - r + 1);
+            System.arraycopy(array, r, tmp, t, right - r);
         }
 
-        System.arraycopy(tmp, 0, array, left, right - left + 1);
+        System.arraycopy(tmp, left, array, left, right - left);
     }
 }
