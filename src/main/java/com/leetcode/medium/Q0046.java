39085f73e4bf68d7ec6de536b9004d941baafc0b
Hyoungho Choi <holuyaa@gmail.com>
2022-05-01 22:05:28 +0900

    Solution - Q0046

---
+++ src/main/java/com/leetcode/medium/Q0046.java
@@ -0,0 +1,29 @@
+package com.leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q0046 {
+    public List<List<Integer>> permute(int[] numbers) {
+        final List<List<Integer>> result = new ArrayList<>();
+        permute(numbers, new boolean[numbers.length], new ArrayList<>(), result, 0);
+        return result;
+    }
+
+    private void permute(int[] numbers, boolean[] visited, List<Integer> tmp, List<List<Integer>> result, int pos) {
+        if (pos == numbers.length) {
+            result.add(new ArrayList<>(tmp));
+            return;
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                tmp.add(numbers[i]);
+                permute(numbers, visited, tmp, result, pos + 1);
+                tmp.remove(tmp.size() - 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
---
+++ src/test/java/com/leetcode/medium/Q0046Test.java
@@ -0,0 +1,39 @@
+package com.leetcode.medium;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Q0046Test {
+
+    private static final int[][] TESTCASES = new int[][]{
+            {1, 2, 3},
+            {0, 1},
+            {1},
+    };
+
+    private static final List<List<List<Integer>>> EXPECTED = List.of(
+            List.of(List.of(1, 2, 3), List.of(1, 3, 2), List.of(2, 1, 3), List.of(2, 3, 1), List.of(3, 1, 2), List.of(3, 2, 1)),
+            List.of(List.of(0, 1), List.of(1, 0)),
+            List.of(List.of(1))
+    );
+
+    @Test
+    void permute() {
+        final Q0046 q = new Q0046();
+        for (int i = 0; i < TESTCASES.length; i++) {
+            final List<List<Integer>> actual = q.permute(TESTCASES[i]);
+            System.out.println(actual);
+            assertListEquals(EXPECTED.get(i), actual);
+        }
+    }
+
+    private void assertListEquals(List<List<Integer>> expected, List<List<Integer>> actual) {
+        assertEquals(expected.size(), actual.size());
+        for (int i = 0; i < expected.size(); i++) {
+            assertEquals(expected.get(i), actual.get(i));
+        }
+    }
+}
