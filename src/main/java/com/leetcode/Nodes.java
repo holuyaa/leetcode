059122636ae2aba43d11cf226599a72e2ed1f194
Hyoungho Choi <holuyaa@gmail.com>
2022-05-16 21:50:48 +0900

    Add to convert from TreeNode to Integer array

--- src/main/java/com/leetcode/Nodes.java
+++ src/main/java/com/leetcode/Nodes.java
@@ -1,5 +1,8 @@
 package com.leetcode;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Nodes {
     public static ListNode listOf(int[] array) {
         if (array == null || array.length == 0) return null;
@@ -25,5 +28,26 @@ public class Nodes {
         node.left = createTree(array, leftIndex);
         node.right = createTree(array, rightIndex);
         return node;
+    }
+
+    public static Integer[] toArray(TreeNode root) {
+        final int count = count(root, 0);
+        final Integer[] result = new Integer[count / 2];
+        toArray(root, result, 0);
+        return result;
+    }
+
+    private static int count(TreeNode node, int pos) {
+        if (node == null) return pos;
+        final int left = count(node.left, (pos * 2) + 1);
+        final int right = count(node.right, (pos + 1) * 2);
+        return Math.max(left, right);
+    }
+
+    private static void toArray(TreeNode node, Integer[] result, int pos) {
+        if (node == null) return;
+        result[pos] = node.val;
+        toArray(node.left, result, (pos * 2) + 1);
+        toArray(node.right, result, (pos + 1) * 2);
     }
 }
