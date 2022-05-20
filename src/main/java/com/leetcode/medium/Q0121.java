deac9762b25b95c5d16588891f077816ec7bcc76
Hyoungho Choi <holuyaa@gmail.com>
2022-05-20 21:22:15 +0900

    Q0121

---
+++ src/main/java/com/leetcode/medium/Q0121.java
@@ -0,0 +1,40 @@
+package com.leetcode.medium;
+
+public class Q0121 {
+    public int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int price : prices) {
+            minPrice = Math.min(minPrice, price);
+            maxProfit = Math.max(maxProfit, price - minPrice);
+        }
+        return maxProfit;
+    }
+
+    public int maxProfit1(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+                continue;
+            }
+            maxProfit = Math.max(maxProfit, price - minPrice);
+        }
+        return maxProfit;
+    }
+
+    public int maxProfit2(int[] prices) {
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            final int price = prices[i];
+            if (price < minPrice) {
+                minPrice = price;
+                continue;
+            }
+            maxProfit = Math.max(maxProfit, price - minPrice);
+        }
+        return maxProfit;
+    }
+}
---
+++ src/test/java/com/leetcode/medium/Q0121Test.java
@@ -0,0 +1,47 @@
+package com.leetcode.medium;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.function.ToIntFunction;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+class Q0121Test {
+
+    private static final int[][] TESTCASES = new int[][]{
+            {7,1,5,3,6,4},
+            {7,6,4,3,1},
+    };
+
+    private static final int[] EXPECTED = new int[]{
+           5,
+           0,
+    };
+
+    @Test
+    void maxProfit() {
+        final Q0121 q = new Q0121();
+        maxProfit(q::maxProfit);
+    }
+
+    @Test
+    void maxProfit1() {
+        final Q0121 q = new Q0121();
+        maxProfit(q::maxProfit1);
+    }
+
+    @Test
+    void maxProfit2() {
+        final Q0121 q = new Q0121();
+        maxProfit(q::maxProfit2);
+    }
+
+    private void maxProfit(ToIntFunction<int[]> func) {
+        for (int i = 0; i < TESTCASES.length; i++) {
+            final int actual = func.applyAsInt(TESTCASES[i]);
+            System.out.println(actual);
+            assertThat(actual, is(EXPECTED[i]));
+        }
+    }
+}
