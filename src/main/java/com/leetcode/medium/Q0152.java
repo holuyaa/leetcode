5e2e9cf4618452d5f8ee623933877015b3b894e2
Hyoungho Choi <holuyaa@gmail.com>
2022-06-03 08:31:15 +0900

    Q0152

--- src/main/java/com/leetcode/medium/Q0152.java
+++ src/main/java/com/leetcode/medium/Q0152.java
@@ -8,15 +8,29 @@ public class Q0152 {
         int minProduct = max;
         for (int i = 1; i < len; i++) {
             final int num = numbers[i];
-           if (num < 0) {
-               final int tmp = maxProduct;
-               maxProduct = Math.max(minProduct * num, num);
-               minProduct = Math.min(tmp * num, num);
-           } else {
-               maxProduct = Math.max(maxProduct * num, num);
-               minProduct = Math.min(minProduct * num, num);
-           }
-           max = Math.max(max, maxProduct);
+            if (num < 0) {
+                final int tmp = maxProduct;
+                maxProduct = Math.max(minProduct * num, num);
+                minProduct = Math.min(tmp * num, num);
+            } else {
+                maxProduct = Math.max(maxProduct * num, num);
+                minProduct = Math.min(minProduct * num, num);
+            }
+            max = Math.max(max, maxProduct);
+        }
+        return max;
+    }
+
+    public int maxProduct1(int[] numbers) {
+        final int len = numbers.length;
+        int max = numbers[0];
+        int left = 0;
+        int right = 0;
+
+        for (int i = 0; i < len; i++) {
+            left = (left == 0 ? 1 : left) * numbers[i];
+            right = (right == 0 ? 1 : right) * numbers[len - 1 - i];
+            max = Math.max(max, Math.max(left, right));
         }
         return max;
     }
--- src/test/java/com/leetcode/medium/Q0152Test.java
+++ src/test/java/com/leetcode/medium/Q0152Test.java
@@ -2,6 +2,8 @@ package com.leetcode.medium;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.function.ToIntFunction;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -28,8 +30,18 @@ class Q0152Test {
     @Test
     void maxProduct() {
         final Q0152 q = new Q0152();
+        maxProduct(q::maxProduct);
+    }
+
+    @Test
+    void maxProduct1() {
+        final Q0152 q = new Q0152();
+        maxProduct(q::maxProduct1);
+    }
+
+    private void maxProduct(ToIntFunction<int[]> func) {
         for (int i = 0; i < TESTCASES.length; i++) {
-            final int actual = q.maxProduct(TESTCASES[i]);
+            final int actual = func.applyAsInt(TESTCASES[i]);
             System.out.println(actual);
             assertThat(actual, is(EXPECTED[i]));
         }
