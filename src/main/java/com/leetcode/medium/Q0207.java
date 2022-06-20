bb602adaab5f071f64d7c7cd296baa82af5617e7
Hyoungho Choi <holuyaa@gmail.com>
2022-06-20 23:16:50 +0900

    Q0207

--- src/main/java/com/leetcode/medium/Q0207.java
+++ src/main/java/com/leetcode/medium/Q0207.java
@@ -1,14 +1,14 @@
 package com.leetcode.medium;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class Q0207 {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int count = 0;
         final int[] degree = new int[numCourses];
         final boolean[][] table = new boolean[numCourses][numCourses];
-        final boolean[] visited = new boolean[numCourses];
         final Queue<Integer> queue = new LinkedList<>();
 
         for (int[] prerequisite : prerequisites) {
@@ -19,21 +19,46 @@ public class Q0207 {
         }
 
         for (int i = 0; i < numCourses; i++) {
-            if (degree[i] != 0) continue;
-            visited[i] = true;
-            queue.offer(i);
+            if (degree[i] == 0) queue.offer(i);
         }
 
+        int count = numCourses;
         while (!queue.isEmpty()) {
             final int v = queue.poll();
-            count++;
+            count--;
             for (int i = 0; i < numCourses; i++) {
-                if (table[v][i] && !visited[i] && --degree[i] == 0) {
-                    visited[i] = true;
+                if (table[v][i] && --degree[i] == 0) {
                     queue.offer(i);
                 }
             }
         }
-        return count == numCourses;
+        return count == 0;
+    }
+
+    public boolean canFinish1(int numCourses, int[][] prerequisites) {
+        final int[] degree = new int[numCourses];
+        final List<List<Integer>> outgoing = new ArrayList<>();
+        for (int i = 0; i < numCourses; i++) outgoing.add(new LinkedList<>());
+        for (int[] prerequisite : prerequisites) {
+            final int from = prerequisite[1];
+            final int to = prerequisite[0];
+            degree[to]++;
+            outgoing.get(from).add(to);
+        }
+
+        final Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < degree.length; i++) {
+            if (degree[i] == 0) queue.offer(i);
+        }
+
+        int count = prerequisites.length;
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            for (int v : outgoing.get(cur)) {
+                count--;
+                if (--degree[v] == 0) queue.offer(v);
+            }
+        }
+        return count == 0;
     }
 }
--- src/test/java/com/leetcode/medium/Q0207Test.java
+++ src/test/java/com/leetcode/medium/Q0207Test.java
@@ -2,6 +2,8 @@ package com.leetcode.medium;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.function.BiFunction;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -10,23 +12,36 @@ class Q0207Test {
     private static final int[] NUM_COURSES = new int[]{
             2,
             2,
+            2,
     };
 
     private static final int[][][] TESTCASES = new int[][][]{
             {{1, 0}},
             {{1, 0}, {0, 1}},
+            {{0, 1}},
     };
 
     private static final boolean[] EXPECTED = new boolean[]{
             true,
             false,
+            true,
     };
 
     @Test
     void canFinish() {
         final Q0207 q = new Q0207();
+        canFinish(q::canFinish);
+    }
+
+    @Test
+    void canFinish1() {
+        final Q0207 q = new Q0207();
+        canFinish(q::canFinish1);
+    }
+
+    private void canFinish(BiFunction<Integer, int[][], Boolean> func) {
         for (int i = 0; i < TESTCASES.length; i++) {
-            final boolean actual = q.canFinish(NUM_COURSES[i], TESTCASES[i]);
+            final boolean actual = func.apply(NUM_COURSES[i], TESTCASES[i]);
             System.out.println(actual);
             assertThat(actual, is(EXPECTED[i]));
         }
